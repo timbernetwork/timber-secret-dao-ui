0ad9fa9fd72560f7d97ef85b711d1bbb534c59b7
Matthew Nolan <matthewnolan415@gmail.com>
2022-12-20 20:50:02 +0700

    content

---
+++ ui/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/daopage":["static\u002Fchunks\u002Fpages\u002Fdaopage.js"],"/relay":["static\u002Fchunks\u002Fpages\u002Frelay.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdaopage","\u002Frelay"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
